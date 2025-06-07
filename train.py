c7822f8304f9f1a128e075abe610c193cfeb0b2d
RukawaY <rukawayuan@outlook.com>
2025-06-07 18:48:02 +0800

    update gitignore and train.py

--- train.py
+++ train.py
@@ -72,7 +72,8 @@ def get_runner_class(cfg):
 def main():
     # allow auto-dl completes on main process without timeout when using NCCL backend.
     # os.environ["NCCL_BLOCKING_WAIT"] = "1"
-
+    print(torch.cuda.is_available())
+    torch.cuda.empty_cache()
     # set before init_distributed_mode() to ensure the same job_id shared across all ranks.
     job_id = now()
     args = parse_args()
